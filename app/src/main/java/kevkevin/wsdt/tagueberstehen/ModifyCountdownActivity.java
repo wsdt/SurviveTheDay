fc795bf5abe01606d11f738ceb328967ce45aa9b
kevin <kevin.riedl.privat@gmail.com>
2017-12-23 23:57:56 +0100

    started modify countdown act

---
+++ app/src/main/java/kevkevin/wsdt/tagueberstehen/ModifyCountdownActivity.java
@@ -0,0 +1,15 @@
+package kevkevin.wsdt.tagueberstehen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ModifyCountdownActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modify_countdown);
+
+        //TODO: createCountdownAct.putExtra("CRUD","C"); --> get extra each letter (except read)
+    }
+}
