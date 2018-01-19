b2582aceafbf024605222f5a43d23631261f6268
kevin <kevin.riedl.privat@gmail.com>
2018-01-19 22:56:56 +0100

    blinking effect

--- app/src/main/java/kevkevin/wsdt/tagueberstehen/LoadingScreen.java
+++ app/src/main/java/kevkevin/wsdt/tagueberstehen/LoadingScreen.java
@@ -1,9 +1,12 @@
 package kevkevin.wsdt.tagueberstehen;
 
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 
 import kevkevin.wsdt.tagueberstehen.classes.AdManager;
 
@@ -19,6 +22,14 @@ public class LoadingScreen extends AppCompatActivity {
         } catch (NullPointerException e) {
             Log.e(TAG, "onCreate: Nullpointer on Actionbar!");
         }
+
+        //Create blinking effect
+        final ImageView appIcon = ((ImageView) findViewById(R.id.appIcon));
+        ObjectAnimator animator = ObjectAnimator.ofFloat(appIcon, "alpha", 0.25f);
+        animator.setDuration(1000);
+        animator.setRepeatCount(ValueAnimator.INFINITE);
+        animator.setRepeatMode(ValueAnimator.REVERSE);
+        animator.start();
 
         AdManager adManager = new AdManager(this);
         adManager.initializeAdmob();
