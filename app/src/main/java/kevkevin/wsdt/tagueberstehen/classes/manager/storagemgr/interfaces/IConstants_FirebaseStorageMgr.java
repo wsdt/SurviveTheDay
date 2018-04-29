376442ee37457abb7fcc1ecaa6b67033c59c3eae
kevin <kevin.riedl.privat@gmail.com>
2018-04-29 20:12:46 +0200

    small things

--- app/src/main/java/kevkevin/wsdt/tagueberstehen/classes/manager/storagemgr/interfaces/IConstants_FirebaseStorageMgr.java
+++ app/src/main/java/kevkevin/wsdt/tagueberstehen/classes/manager/storagemgr/interfaces/IConstants_FirebaseStorageMgr.java
@@ -5,4 +5,9 @@ public interface IConstants_FirebaseStorageMgr {
      * the FirebaseStorageMgr.class, so the newly saved Userlibrary gets saved correctly for the specific version code! */
     String LIB_JSON_VERSION_FOLDER = "v1"; //versioning, so we can change the firebaseJson over time without any problems
     String LIB_FILEEXTENSION = "json";
+
+    interface INDEX_FILES {
+        String FILENAME = "__index"; //all index files on Firebase need to be named like this!
+        String FILETYPE = "json";
+    }
 }
