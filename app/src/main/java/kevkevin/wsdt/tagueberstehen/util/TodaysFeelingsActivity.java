b3bed3a32b144d24d2addbfc13d897e5896aab39
Solution49 <crisjonny@live.at>
2018-05-15 16:04:48 +0200

    created schema for feelingActivity, with placeholders!

---
+++ app/src/main/java/kevkevin/wsdt/tagueberstehen/util/TodaysFeelingsActivity.java
@@ -0,0 +1,15 @@
+package kevkevin.wsdt.tagueberstehen.util;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import kevkevin.wsdt.tagueberstehen.R;
+
+public class TodaysFeelingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_todays_feelings);
+    }
+}
